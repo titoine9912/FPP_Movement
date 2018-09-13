dd2405d630a2e3444781eb950e16c58edfaaade5
Kasandra Brassard <1463457@etu.cegep-ste-foy.qc.ca>
2018-09-13 08:50:23 -0400

    commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script
+{
+	public class Player : MonoBehaviour
+	{
+		private Rigidbody2D rgBody;
+		private float speed = 1;
+		
+		public void Start()
+		{
+			rgBody = GetComponent<Rigidbody2D>();
+		}
+
+		public void FixedUpdate()
+		{
+			float moveHorizontal = Input.GetAxis("Horizontal");
+			float moveVertical = Input.GetAxis("Vertical");
+			
+			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+			
+			rgBody.AddForce(movement * speed);
+		}
+	}
+}
