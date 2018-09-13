7ee81dae7f798dd73f708a764af6b75687eb63ee
antoine Roy-Lachance <titoine9912@hotmail.com>
2018-09-13 10:21:46 -0400

    ajout PlayerController

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,7 +1,22 @@
-﻿namespace Script
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Script
 {
-	public class PlayerController
+	public class PlayerController:MonoBehaviour
 	{
-		
+        float speed = 1.5f;
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+        }
 	}
 }
