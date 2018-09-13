dff3732c7e758263ac2dbecda48baf67bc411111
Kasandra Brassard <1463457@etu.cegep-ste-foy.qc.ca>
2018-09-13 10:21:42 -0400

    player

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 namespace Script
 {
 	public class Player : MonoBehaviour
 	{
 		private Rigidbody2D rgBody;
-		private float speed = 1;
+		private float speed = 5;
 		
 		public void Start()
 		{
@@ -21,6 +20,21 @@ namespace Script
 			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 			
 			rgBody.AddForce(movement * speed);
+		}
+
+		public void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.A))
+			{
+				//rgBody.MovePosition(new Vector3(rgBody.position.x - 1 * speed, rgBody.position.y));
+				rgBody.transform.Translate(new Vector3(rgBody.position.x - 1 * speed, rgBody.position.y));
+			}
+			
+			if (Input.GetKeyDown(KeyCode.D))
+			{
+				//rgBody.MovePosition(new Vector3(rgBody.position.x + 1 * speed, rgBody.position.y));
+				rgBody.transform.Translate(new Vector3(rgBody.position.x + 1 * speed, rgBody.position.y));
+			}
 		}
 	}
 }
