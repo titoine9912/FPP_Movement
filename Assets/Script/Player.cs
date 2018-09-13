c8f1f62355d0fec38b5ae72274bdfad98b6e81a8
Kasandra Brassard <1463457@etu.cegep-ste-foy.qc.ca>
2018-09-13 12:29:54 -0400

    player

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,7 +5,7 @@ namespace Script
 	public class Player : MonoBehaviour
 	{
 		private Rigidbody2D rgBody;
-		private float speed = 5;
+		private float speed = 5f;
 		
 		public void Start()
 		{
@@ -14,27 +14,61 @@ namespace Script
 
 		public void FixedUpdate()
 		{
-			float moveHorizontal = Input.GetAxis("Horizontal");
+			/*float moveHorizontal = Input.GetAxis("Horizontal");
 			float moveVertical = Input.GetAxis("Vertical");
 			
 			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 			
-			rgBody.AddForce(movement * speed);
+			rgBody.AddForce(movement * speed);*/
 		}
 
 		public void Update()
 		{
-			if (Input.GetKeyDown(KeyCode.A))
+			var targetVelocity = Vector2.zero;
+			var force = 0.5f;
+			
+			if (Input.GetKey(KeyCode.A))
+			{
+				targetVelocity += Vector2.left * speed;
+			}
+			if (Input.GetKey(KeyCode.D))
+			{
+				targetVelocity += Vector2.right * speed;
+			}
+			if (Input.GetKey(KeyCode.Space))
+			{
+				targetVelocity += Vector2.up * speed * 10;
+			}
+
+			if (targetVelocity.sqrMagnitude > 0)
+				rgBody.velocity = Vector2.Lerp(rgBody.velocity, targetVelocity, Time.deltaTime * force);
+			else
+				rgBody.velocity = Vector2.zero;
+				//rgBody.velocity = Vector2.Lerp(rgBody.velocity, targetVelocity, Time.deltaTime);
+				
+
+			/*
+			if (Input.GetKey(KeyCode.A))
 			{
 				//rgBody.MovePosition(new Vector3(rgBody.position.x - 1 * speed, rgBody.position.y));
-				rgBody.transform.Translate(new Vector3(rgBody.position.x - 1 * speed, rgBody.position.y));
+				//transform.Translate(speed * Time.deltaTime, 0f, 0f);
+				rgBody.velocity = new Vector2(-speed, 0);
 			}
-			
-			if (Input.GetKeyDown(KeyCode.D))
+			if (Input.GetKey(KeyCode.D))
 			{
 				//rgBody.MovePosition(new Vector3(rgBody.position.x + 1 * speed, rgBody.position.y));
-				rgBody.transform.Translate(new Vector3(rgBody.position.x + 1 * speed, rgBody.position.y));
+				rgBody.velocity = new Vector2(speed, 0);
 			}
+			if (Input.GetKey(KeyCode.Space))
+			{
+				rgBody.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+				//rgBody.velocity = new Vector2(0.0f);
+			}
+			if (Input.GetKey(KeyCode.None))
+			{
+				rgBody.velocity = new Vector2(0, 0);
+			}
+			*/
 		}
 	}
 }
